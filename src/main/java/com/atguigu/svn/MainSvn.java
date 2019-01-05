4ec00d87770ebfcc23e89f6500108257275b1c4b
starwu <starwu@atguigu.com>
2019-01-06 00:03:43 +0800

    二次提交到本地库

---
+++ src/main/java/com/atguigu/svn/MainSvn.java
@@ -0,0 +1,21 @@
+package com.atguigu.svn;
+
+public class MainSvn {
+	
+	public static void main(String[] args) {
+		System.out.println("First SVN file");
+		System.out.println("From SVN");
+		System.err.println("LocalHost中添加SVN");
+		System.err.println("LocalHost中添加SVN");
+		System.err.println("HelloChina!!---left");
+		System.err.println("HelloWorld!!");
+		System.out.println("HelloShenzhen!!");
+		System.err.println("LocalHost中添加SVN-------------right");
+		System.err.println("LocalHost中添加SVN---------left");
+		System.out.println("bbbbbb......");
+		System.err.println("LocalHost中添加SVN");
+		System.err.println("LocalHost中添加SVN");
+		System.err.println("LocalHost中添加SVN");
+		System.out.println("dddddd..........");
+	}
+}
---
+++ src/test/java/com/atguigu/svn/test/SVNTest.java
@@ -0,0 +1,8 @@
+package com.atguigu.svn.test;
+
+public class SVNTest {
+
+	public static void main(String[] args) {
+		System.out.println("SVNTEST First test!!");
+	}
+}
